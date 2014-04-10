9dc5f2b30fe05db071d17a5c81506729af97b558
Mike Sprague <mike@guidebook.com>
2014-04-09 22:32:25 -0700

    create repo

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+IvPoc::Application.config.secret_key_base = 'eaa9b783dccf05e6a6a1fa85121ce1d2857a6e845797ae50bb403a0e71722105cd8e445d226d261290c687a91c5fd6b5b8fa758ba1db25a2a596014c9c698a14'
