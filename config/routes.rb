33c4c146dc44972eca9653b962f7949f7031efde
Mike Sprague <mike@guidebook.com>
2014-04-15 16:47:52 -0700

    Route to properties, remove a few proerty table fields on index

--- config/routes.rb
+++ config/routes.rb
@@ -10,7 +10,7 @@ IvPoc::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
-
+  root :to => 'properties#index'
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
